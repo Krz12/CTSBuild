811a691c4c8dd7618d3d497b84e8ebc0cef4dd69
Krzysztof Lipiński <krzysiul20082@gmail.com>
2026-03-05 19:41:34 +0100

    FPSY

--- src/performanceStatistics.hpp
+++ src/performanceStatistics.hpp
@@ -3,10 +3,21 @@ using namespace std;
 class performanceStatistics
 {
 private:
-    std::chrono::time_point<std::chrono::high_resolution_clock> lastTime = std::chrono::high_resolution_clock::now(),
-    currentTime = lastTime;
+    std::chrono::time_point<std::chrono::high_resolution_clock> time_frame_end = std::chrono::high_resolution_clock::now(),
+    time_frame_start = time_frame_end;
+    int frames = 0;
+    float __delta_time = 1.0f;
 
 public:
     performanceStatistics();
+    void frame_start()
+    {
+        current_time = std::chrono::high_resolution_clock::now();
+        __delta_time = std::chrono::duration<float>(current_time - last_time).count();
+    }
+    float delta_time()
+    {
+        return __delta_time;
+    }
     ~performanceStatistics();
 };
